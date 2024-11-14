cf7d9a06ed0fbaedc69bb14d1e317cb82cbeeead
Muhammad Usman <142773789+usmanjutt47@users.noreply.github.com>
2024-11-14 15:23:45 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import OnBoarding from "./screens/OnBoarding";
+import { useFonts } from "expo-font";
+import OnBoarding2 from "./screens/OnBoarding2";
+import OnBoarding3 from "./screens/OnBoarding3";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
 
+const Stack = createStackNavigator();
 export default function App() {
+  const [load] = useFonts({
+    "Raleway-Bold": require("./assets/fonts/Raleway-Bold.ttf"),
+    "Raleway-ExtraBold": require("./assets/fonts/Raleway-ExtraBold.ttf"),
+    "Raleway-Medium": require("./assets/fonts/Raleway-Medium.ttf"),
+    "Raleway-Regular": require("./assets/fonts/Raleway-Regular.ttf"),
+    "Raleway-SemiBold": require("./assets/fonts/Raleway-SemiBold.ttf"),
+  });
+  if (!load) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="OnBoarding"
+          component={OnBoarding}
+          options={{ ...TransitionPresets.SlideFromRightIOS }}
+        />
+        <Stack.Screen
+          name="OnBoarding2"
+          component={OnBoarding2}
+          options={{ ...TransitionPresets.SlideFromRightIOS }}
+        />
+        <Stack.Screen
+          name="OnBoarding3"
+          component={OnBoarding3}
+          options={{ ...TransitionPresets.SlideFromRightIOS }}
+        />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ ...TransitionPresets.ModalPresentationIOS }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ ...TransitionPresets.ModalPresentationIOS }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
